ce0ebfcd6b201a94bad12bbb871f8aa1b43d11ff
in6004 <Insa_Kruse@web.de>
2017-06-03 00:41:52 +0200

    Fredericks TEST classes

--- KaufDort_Cluster/src/backend/NumericFeatureTEST.java
+++ KaufDort_Cluster/src/backend/NumericFeatureTEST.java
@@ -1,5 +1,70 @@
 package backend;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class NumericFeatureTEST {
 
+	NumericFeature testObjekt1;
+	NumericFeature testObjekt2;
+	ArrayList<HashMap<Double, Integer>> testList1;
+	ArrayList<HashMap<Double, Integer>> testList2;
+	
+	
+	@Before
+	public void setUp(){
+		testObjekt1 = new NumericFeature("Futter",4,6);
+		testObjekt2 = new NumericFeature("Trinken",8,6);
+		testList1 = new ArrayList<HashMap<Double, Integer>>();
+		testList2 = new ArrayList<HashMap<Double, Integer>>();
+		testObjekt1.addFeatureElementForCluster(1,1.1);
+		testObjekt1.addFeatureElementForCluster(2,2.2);
+		testObjekt1.addFeatureElementForCluster(3,3.3);
+		testObjekt1.addFeatureElementForCluster(4,4.4);
+		testObjekt1.addFeatureElementForCluster(5,5.5);
+		testObjekt2.addFeatureElementForCluster(1,6.6);
+		testObjekt2.addFeatureElementForCluster(2,7.7);
+		testObjekt2.addFeatureElementForCluster(3,8.8);
+		testObjekt2.addFeatureElementForCluster(4,9.9);
+		testObjekt2.addFeatureElementForCluster(5,10.1);
+		
+		for (int i = 0; i < 6; i++) {
+			testList1.add(new HashMap<Double, Integer>());
+			testList2.add(new HashMap<Double, Integer>());
+		}
+
+		testList1.get(1).put(1.1, 1);
+		testList1.get(2).put(2.2, 1);
+		testList1.get(3).put(3.3, 1);
+		testList1.get(4).put(4.4, 1);
+		testList1.get(5).put(5.5, 1);
+		testList2.get(1).put(6.6, 1);
+		testList2.get(2).put(7.7, 1);
+		testList2.get(3).put(8.8, 1);
+		testList2.get(4).put(9.9, 1);
+		testList2.get(5).put(10.1, 1);
+	}
+	
+	
+	@Test
+	public void testNumericFeatureAttributes(){		
+		assertEquals("Futter",testObjekt1.getFeatureName());
+		assertEquals(4,testObjekt1.getFeatureType());
+		assertEquals(6,testObjekt1.getAllClusterData().size());
+		assertEquals("Trinken",testObjekt2.getFeatureName());
+		assertEquals(8,testObjekt2.getFeatureType());
+		assertEquals(6,testObjekt2.getAllClusterData().size());
+	}
+	
+	@Test
+	public void testaddFeatureElementForCluster(){
+		assertEquals(testList1,testObjekt1.getAllClusterData());
+		assertEquals(testList2,testObjekt2.getAllClusterData());
+		
+	}
 }
--- KaufDort_Cluster/src/backend/WekaClusteringTEST.java
+++ KaufDort_Cluster/src/backend/WekaClusteringTEST.java
@@ -1,5 +1,58 @@
 package backend;
 
+import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import backend.WekaClustering;
+
 public class WekaClusteringTEST {
 
+	WekaClustering testObjekt;
+	String testObjektPfad;
+	BufferedReader read;
+	String temp;
+	String ergebnis;
+	String[] ergebnisString;
+	ArrayList <String> testFeatures;
+	ArrayList <String> testObjektFeatures;
+	
+	@Before
+	public void setUp() throws IOException{
+		testObjektPfad = "C:/Users/Frederik/Downloads/eclipse/workspace/MusterloesungWeka.csv"; //testdateien müssen eingefügt werden
+		testObjekt = new WekaClustering("C:/Users/Frederik/Downloads/eclipse/workspace/MusterloesungWeka.arff",5);
+		read = createFileReader(testObjektPfad);
+		ergebnis = "";
+		testFeatures = new ArrayList<String>();
+		testObjektFeatures = new ArrayList<String>();
+		
+		while((temp = read.readLine()) != null)
+			ergebnis = ergebnis + temp;
+		ergebnisString = ergebnis.split(",");
+		System.out.println(ergebnisString[23]);
+		
+		for(int i=0;i<testFeatures.size();i++)
+			testFeatures.add(ergebnisString[i]);
+		
+		for(int i=0;i<testFeatures.size();i++)
+			testObjektFeatures.add(testObjekt.getFeatureList().get(i).getFeatureName());
+	}
+	
+	@Test
+	public void testWekaClustering(){
+		assertEquals(testFeatures,testObjektFeatures);
+	}
+	
+	
+	private BufferedReader createFileReader(String pfad) throws FileNotFoundException,	UnsupportedEncodingException {
+		return new BufferedReader(new InputStreamReader(new FileInputStream(
+		pfad), "windows-1252"));
+	}
+	
 }
